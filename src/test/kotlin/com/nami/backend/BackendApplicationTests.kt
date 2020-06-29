d6ba2c0fb38b5fcaa2319639eda7993e44da6063
lunylein <lunylein@gmail.com>
2020-06-29 23:57:10 +0200

    add tests

---
+++ src/test/kotlin/com/nami/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nami.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
