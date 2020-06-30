669b98a76f2e60fc98f2400850a721539d13f4ba
Siroque <>
2020-07-01 00:25:43 +0200

    NOTICKET Rework gradle build file, add version logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,31 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.0.RELEASE"
+	val kotlinVersion = "1.3.72"
+	id("org.springframework.boot") version "2.3.1.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.72"
-	kotlin("plugin.spring") version "1.3.72"
-	kotlin("plugin.noarg") version "1.3.72"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.jpa") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
 }
-apply{
-	plugin("kotlin")
-	plugin("kotlin-spring")
-	plugin("kotlin-jpa")
-	plugin("kotlin-noarg")
-}
+
+val kotlinVersion = project.property("kotlinVersion")
 val gitVersion: String? = "git rev-list HEAD --count".runCommand(file("$rootDir"))
 
 group = "com.nami"
 version = "0.1-$gitVersion"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+logger.lifecycle("Versions: project <$version>, java <${System.getProperty("java.version")}>, " +
+		"kotlin <${project.property("kotlinVersion")}>, " +
+		"gradle <${project.gradle.gradleVersion}>, groovy <${groovy.lang.GroovySystem.getVersion()}>")
+
 repositories {
 	mavenCentral()
 	jcenter()
 	maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
-buildscript {
-	dependencies {
-		classpath(kotlin("gradle-plugin", "1.3.72"))
-		classpath(kotlin("allopen", "1.3.72"))
-		classpath(kotlin("noarg", "1.3.72"))
-		classpath("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.3.72")
-	}
-}
-allOpen {
-	annotation("javax.persistence.Entity")
-	annotation("javax.persistence.MappedSuperclass")
-	annotation("javax.persistence.Embeddable")
-}
+
 dependencies {
 	//implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	//implementation("org.springframework.boot:spring-boot-starter-security")
@@ -47,11 +36,6 @@ dependencies {
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.squareup.retrofit2:retrofit:2.3.0")
-	implementation("com.squareup.retrofit2:adapter-rxjava2:2.3.0")
-	implementation("com.squareup.retrofit2:converter-gson:2.3.0")
-	implementation("io.reactivex.rxjava2:rxandroid:2.0.1")
-	implementation("com.zaxxer:HikariCP:3.4.5")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
