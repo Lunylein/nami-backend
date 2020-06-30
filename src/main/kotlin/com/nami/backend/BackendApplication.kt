77a2212581212df11bb1822846bf92cb74aea499
lunylein <lunylein@gmail.com>
2020-06-30 21:20:37 +0200

    removed unused imports

--- src/main/kotlin/com/nami/backend/BackendApplication.kt
+++ src/main/kotlin/com/nami/backend/BackendApplication.kt
@@ -2,9 +2,6 @@ package com.nami.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.RestController
-import java.net.URI
-import java.net.http.HttpRequest
 
 @SpringBootApplication
 class BackendApplication
