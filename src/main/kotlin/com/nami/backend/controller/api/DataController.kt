718f4002f005cc07513e35b7275b229b8481e900
lunylein <lunylein@gmail.com>
2020-06-30 22:34:25 +0200

    extracted kocj-data retrieval to dataService

--- src/main/kotlin/com/nami/backend/controller/api/DataController.kt
+++ src/main/kotlin/com/nami/backend/controller/api/DataController.kt
@@ -13,21 +13,12 @@ import java.time.format.DateTimeFormatter
 @RestController
 @RequestMapping("api/v1")
 class DataController {
-  /*      private val DataService: dataService? = null
-
         @Autowired
-        fun DataController(productService: DataService?) {
-                this.dataService = dataService
-        }*/
+        private lateinit var dataService: DataService
+
         @GetMapping(path = ["/{serie}"])
         fun series (@PathVariable("serie") serie: String): List<Serie> {
-                val list = mutableListOf<Serie>()
-                list.add(Serie("f1", "url", LocalDateTime.now()))
-                list.add(Serie("f1", "url1", LocalDateTime.now()))
-                list.add(Serie("f1", "url2", LocalDateTime.now()))
-                list.add(Serie("f1", "url3", LocalDateTime.now()))
-                list.add(Serie("f1", "url4", LocalDateTime.now()))
-                return list.filter { it.series == serie };
+                return dataService.series(serie).filter { it.series == serie };
         }
 
 }
--- src/main/kotlin/com/nami/backend/service/DataService.kt
+++ src/main/kotlin/com/nami/backend/service/DataService.kt
@@ -1,16 +1,27 @@
 package com.nami.backend.service
 
 import com.nami.backend.dao.SerieDao
+import com.nami.backend.model.Serie
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
+import java.time.LocalDateTime
 
 @Service
-class DataService {
-    private val serieDao: SerieDao? = null
+@Component
+class DataService{
 
-    @Autowired
-    fun DataService(@Qualifier("fake") serieDao: SerieDao?) {
-     //   this.serieDao = serieDao
+    //private lateinit var serieDao: SerieDao
+
+    fun series (serie: String): List<Serie> {
+        val list = mutableListOf<Serie>()
+        list.add(Serie("f1", "url", LocalDateTime.now()))
+        list.add(Serie("f1", "url1", LocalDateTime.now()))
+        list.add(Serie("f1", "url2", LocalDateTime.now()))
+        list.add(Serie("f1", "url3", LocalDateTime.now()))
+        list.add(Serie("f1", "url4", LocalDateTime.now()))
+        return list;
     }
 }
