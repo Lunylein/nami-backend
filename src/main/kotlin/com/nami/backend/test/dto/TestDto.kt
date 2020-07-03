d6ba570177b8d621ae4a24e06daa590c026ec105
Siroque <donot@bother.me>
2020-07-03 23:51:08 +0200

    NOTICKET add UUID validation to testGetDto

--- src/main/kotlin/com/nami/backend/test/dto/TestDto.kt
+++ src/main/kotlin/com/nami/backend/test/dto/TestDto.kt
@@ -1,9 +1,10 @@
 package com.nami.backend.test.dto
 
+import com.nami.backend.test.UUIDConstraint
 import javax.validation.constraints.Size
 
 data class TestGetDto (
-        val id: String,
+        @UUIDConstraint val id: String,
         @get:Size(max = 125, message = "name to long") val name: String,
         @get:Size(max = 256, message = "description to long") val description: String?
 )
