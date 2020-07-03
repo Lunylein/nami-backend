ee60b8d6b4d72d2001a9a1f68439018c1d1467f2
Siroque <donot@bother.me>
2020-07-03 23:55:10 +0200

    NOTICKET added validator annotation test

--- src/test/kotlin/com/nami/backend/BackendApplicationTests.kt
+++ src/test/kotlin/com/nami/backend/BackendApplicationTests.kt
@@ -1,13 +1,33 @@
 package com.nami.backend
 
+import com.nami.backend.test.dto.TestGetDto
+import com.nami.backend.test.dto.TestPostDto
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import javax.validation.ConstraintViolation
+import javax.validation.Validator
+
 
 @SpringBootTest
 class BackendApplicationTests {
 
+	@Autowired
+	private val validator: Validator? = null
+
 	@Test
-	fun contextLoads() {
+	@Throws(Exception::class)
+	fun testPostDtoNameShouldContainWordTest() {
+		// given
+		val errTestGetDto = TestGetDto("1", "Name", "description")
+		val testGetDto = TestGetDto("dd07fa45-805a-47da-9dc5-698296cc3cca", "NameTest", "description")
+
+		var violations = validator?.validate(errTestGetDto)
+		// then
+		Assertions.assertEquals(1, violations?.size ?:-1)
+		violations = validator?.validate(testGetDto)
+		Assertions.assertEquals(0, violations?.size ?:-1)
 	}
 
 }
