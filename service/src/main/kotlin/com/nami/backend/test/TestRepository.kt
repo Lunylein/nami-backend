91296024c461b320cef6ec78d44872212671f168
Siroque <donot@bother.me>
2020-07-08 20:34:39 +0200

    NOTICKET Remove unnecessay annotation

--- service/src/main/kotlin/com/nami/backend/test/TestRepository.kt
+++ service/src/main/kotlin/com/nami/backend/test/TestRepository.kt
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
-@Repository
 interface TestRepository : JpaRepository<TestEntity, UUID>
